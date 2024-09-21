fb76591a5b1e8e583fc635b0c166f226dab2ecab
itanstar <thsdmlwls55@naver.com>
2024-09-21 19:06:50 +0900

    a

---
+++ src/components/BlackCat.js
@@ -0,0 +1,12 @@
+// src/components/GifDisplay.js
+import React from 'react';
+
+const BlackCat = () => {
+  return (
+    <div>
+      <img src="/blackcat.gif" alt="My blackcat" style={{ width: '600px' }} />
+    </div>
+  );
+};
+
+export default BlackCat;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,15 @@
 // src/pages/Home.js
 import React from 'react';
+import BlackCat from '../components/BlackCat';
 
 const Main = () => {
   return (
     <div>
-      <h1>How to recycle in Korea?</h1>
-      <p>환영합니다! 여기에 홈 페이지 내용을 추가하세요.</p>
+      <h1>How waste is managed in South Korea</h1>
+      <BlackCat />
+      <p style={{ fontSize: '1.2em' }}>I created this webpage to help foreign students understand how to properly separate and dispose of waste in South Korea.</p>
+      <p style={{ fontSize: '1.2em' }}>Waste management practices can vary significantly from country to country, and it's important for newcomers to learn the local rules to contribute to a cleaner environment. </p>
+      <p style={{ fontSize: '1.2em' }}>This site provides clear guidelines on the different types of waste, recycling procedures, and tips for responsible disposal, ensuring that everyone can participate in keeping our community clean and sustainable.</p>
     </div>
   );
 };
